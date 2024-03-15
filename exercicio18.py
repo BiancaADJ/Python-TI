dda753dfd3429c93a452526534d02346edc876e3
Brytanni <99222703+BrytanniADJ@users.noreply.github.com>
2024-03-15 16:29:43 -0300

    up

--- exercicio18.py
+++ exercicio18.py
@@ -4,6 +4,6 @@ latas: float = litros / 18
 razao = round(latas)
 resto = latas - razao # fazendo as contas das latas e o resto
 
-if resto > 0:
+if resto != 0:
     razao = razao + 1 # adicionando uma lata a mais caso tenha resto
 print(f'A quantidade de latas que você precisará comprar é: {razao}\nE você terá que pagar: R${razao * 80}') # exibindo resultado
